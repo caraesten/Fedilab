2f321c2f17d6056bd9d720bd4c548d1874e319e8
tom79 <tschneider.ac@gmail.com>
2017-08-14 19:07:07 +0200

    Adds URL when a media is added

--- app/src/main/java/fr/gouv/etalab/mastodon/activities/TootActivity.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/activities/TootActivity.java
@@ -788,6 +788,12 @@ public class TootActivity extends AppCompatActivity implements OnRetrieveSearcAc
             imParams.height = (int) Helper.convertDpToPixel(100, getApplicationContext());
             imageView.setAdjustViewBounds(true);
             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            //Adds the url at the end of the toot 
+            int selectionBefore = toot_content.getSelectionStart();
+            toot_content.setText(toot_content.getText().toString()+"\n"+attachment.getUrl());
+            //Moves the cursor
+            if( selectionBefore >= 0 )
+                toot_content.setSelection(selectionBefore);
             toot_picture_container.addView(imageView, attachments.size(), imParams);
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -826,6 +832,12 @@ public class TootActivity extends AppCompatActivity implements OnRetrieveSearcAc
                 for(Attachment attachment: attachments){
                     if( Integer.valueOf(attachment.getId()) == viewId){
                         attachments.remove(attachment);
+                        //Adds the url at the end of the toot 
+                        int selectionBefore = toot_content.getSelectionStart();
+                        toot_content.setText(toot_content.getText().toString().replace(attachment.getUrl(),""));
+                        //Moves the cursor
+                        if( selectionBefore >= 0 )
+                            toot_content.setSelection(selectionBefore);
                         ((ViewGroup) namebar.getParent()).removeView(namebar);
                         break;
                     }
