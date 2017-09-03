6c5e50a5ca710037ee1ef04f95ff9bebbd6f7b98
PhotonQyv <shenstone.porter@gmail.com>
2017-09-03 23:30:56 +0100

    Updated code to work if URL is embedded within other text in sharedText.

--- app/src/fdroid/java/fr.gouv.etalab.mastodon/activities/MainActivity.java
+++ app/src/fdroid/java/fr.gouv.etalab.mastodon/activities/MainActivity.java
@@ -611,12 +611,16 @@ public class MainActivity extends AppCompatActivity
                     /* Some apps don't send the URL as the first part of the EXTRA_TEXT,
                         the BBC News app being one such, in this case find where the URL
                         is and strip that out into sharedText.
-
-                        TODO: ASSUMES that URL is at end of sharedText!
                      */
                     if (!sharedText.startsWith("http")) {
                         int index = sharedText.indexOf("http");
-                        sharedText= sharedText.substring(index);
+                        if (index > -1)
+                            sharedText = sharedText.substring(index);
+
+                        index = sharedText.indexOf(' ');
+
+                        if (index > -1)
+                            sharedText = sharedText.substring(0, index -1);
                     }
                     new RetrieveMetaDataAsyncTask(sharedText, MainActivity.this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                     Intent intentToot = new Intent(getApplicationContext(), TootActivity.class);
