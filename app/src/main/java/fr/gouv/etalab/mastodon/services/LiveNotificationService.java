2f48281c25262f4343a84182520e0899058fc9c2
stom79 <tschneider.ac@gmail.com>
2017-11-30 18:43:47 +0100

    Improves streaming

--- app/src/main/java/fr/gouv/etalab/mastodon/services/LiveNotificationService.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/services/LiveNotificationService.java
@@ -25,7 +25,6 @@ import android.os.IBinder;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 import android.view.View;
 
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
