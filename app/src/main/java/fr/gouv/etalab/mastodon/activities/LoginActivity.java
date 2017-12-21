ef4061fd4485359ed729f403f54ad3199212d93f
stom79 <tschneider.ac@gmail.com>
2017-12-21 13:29:51 +0100

    Fix email issue with special chars

--- app/src/main/java/fr/gouv/etalab/mastodon/activities/LoginActivity.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/activities/LoginActivity.java
@@ -263,7 +263,11 @@ public class LoginActivity extends BaseActivity {
                 parameters.put(Helper.CLIENT_ID, sharedpreferences.getString(Helper.CLIENT_ID, null));
                 parameters.put(Helper.CLIENT_SECRET, sharedpreferences.getString(Helper.CLIENT_SECRET, null));
                 parameters.put("grant_type", "password");
-                parameters.put("username",login_uid.getText().toString().trim());
+                try {
+                    parameters.put("username",URLEncoder.encode(login_uid.getText().toString().trim(), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    parameters.put("username",login_uid.getText().toString().trim());
+                }
                 try {
                     parameters.put("password",URLEncoder.encode(login_passwd.getText().toString(), "UTF-8"));
                 } catch (UnsupportedEncodingException e) {
