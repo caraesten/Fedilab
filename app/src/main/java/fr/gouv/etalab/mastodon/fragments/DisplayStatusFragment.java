b45cf8be1c28595d308ba97865cd8a13edc00050
stom79 <tschneider.ac@gmail.com>
2018-01-02 11:42:11 +0100

    Improves error messages

--- app/src/main/java/fr/gouv/etalab/mastodon/fragments/DisplayStatusFragment.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/fragments/DisplayStatusFragment.java
@@ -253,9 +253,9 @@ public class DisplayStatusFragment extends Fragment implements OnRetrieveFeedsIn
         mainLoader.setVisibility(View.GONE);
         nextElementLoader.setVisibility(View.GONE);
         //Discards 404 - error which can often happen due to toots which have been deleted
-        if( apiResponse.getError() != null ){
+        if( apiResponse.getError() != null && apiResponse.getError().getStatusCode() != 404 ){
             boolean show_error_messages = sharedpreferences.getBoolean(Helper.SET_SHOW_ERROR_MESSAGES, true);
-            if( show_error_messages && !apiResponse.getError().getError().startsWith("404 -"))
+            if( show_error_messages )
                 Toast.makeText(context, apiResponse.getError().getError(),Toast.LENGTH_LONG).show();
             swipeRefreshLayout.setRefreshing(false);
             swiped = false;
