14ab8846224f3b37d32f9f00f3d3bcd136626f4f
stom79 <tschneider.ac@gmail.com>
2018-09-23 11:20:39 +0200

    Fix notification issue when device is set in silent mode for Android 8+

--- app/src/main/java/fr/gouv/etalab/mastodon/helper/Helper.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/helper/Helper.java
@@ -888,7 +888,7 @@ public class Helper {
                 String soundUri = sharedpreferences.getString(Helper.SET_NOTIF_SOUND, ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() +"/"+ R.raw.boop);
                 AudioAttributes audioAttributes = new AudioAttributes.Builder()
                         .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                        .setUsage(AudioAttributes.USAGE_ALARM)
+                        .setUsage(AudioAttributes.USAGE_NOTIFICATION)
                         .build();
                 channel.setSound(Uri.parse(soundUri), audioAttributes);
             }
