545b6ccb7c05ecd715adc19e30704bddb5ba92bf
stom79 <tschneider.ac@gmail.com>
2019-02-18 14:00:00 +0100

    Fix pagination issue

--- app/src/main/java/fr/gouv/etalab/mastodon/client/HttpsConnection.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/client/HttpsConnection.java
@@ -1706,13 +1706,13 @@ public class HttpsConnection {
             Map<String, List<String>> map = httpsURLConnection.getHeaderFields();
             for (Map.Entry<String, List<String>> entry : map.entrySet()) {
                 if (entry.toString().startsWith("Link") || entry.toString().startsWith("link") ) {
-                    Pattern patternMaxId = Pattern.compile("max_id=([0-9]{1,}).*");
+                    Pattern patternMaxId = Pattern.compile("max_id=([0-9a-zA-Z]{1,}).*");
                     Matcher matcherMaxId = patternMaxId.matcher(entry.toString());
                     if (matcherMaxId.find()) {
                         max_id = matcherMaxId.group(1);
                     }
                     if (entry.toString().startsWith("Link")) {
-                        Pattern patternSinceId = Pattern.compile("since_id=([0-9]{1,}).*");
+                        Pattern patternSinceId = Pattern.compile("since_id=([0-9a-zA-Z]{1,}).*");
                         Matcher matcherSinceId = patternSinceId.matcher(entry.toString());
                         if (matcherSinceId.find()) {
                             since_id = matcherSinceId.group(1);
@@ -1727,13 +1727,13 @@ public class HttpsConnection {
             Map<String, List<String>> map = httpURLConnection.getHeaderFields();
             for (Map.Entry<String, List<String>> entry : map.entrySet()) {
                 if (entry.toString().startsWith("Link") || entry.toString().startsWith("link")) {
-                    Pattern patternMaxId = Pattern.compile("max_id=([0-9]{1,}).*");
+                    Pattern patternMaxId = Pattern.compile("max_id=([0-9a-zA-Z]{1,}).*");
                     Matcher matcherMaxId = patternMaxId.matcher(entry.toString());
                     if (matcherMaxId.find()) {
                         max_id = matcherMaxId.group(1);
                     }
                     if (entry.toString().startsWith("Link")) {
-                        Pattern patternSinceId = Pattern.compile("since_id=([0-9]{1,}).*");
+                        Pattern patternSinceId = Pattern.compile("since_id=([0-9a-zA-Z]{1,}).*");
                         Matcher matcherSinceId = patternSinceId.matcher(entry.toString());
                         if (matcherSinceId.find()) {
                             since_id = matcherSinceId.group(1);
