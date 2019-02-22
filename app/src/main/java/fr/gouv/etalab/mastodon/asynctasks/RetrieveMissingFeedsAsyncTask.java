21d7229a1142eb30c9901b93d1632f7bd11a30ae
stom79 <tschneider.ac@gmail.com>
2019-02-22 14:06:39 +0100

    Fix pull to refresh for GNU

--- app/src/main/java/fr/gouv/etalab/mastodon/asynctasks/RetrieveMissingFeedsAsyncTask.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/asynctasks/RetrieveMissingFeedsAsyncTask.java
@@ -28,6 +28,7 @@ import fr.gouv.etalab.mastodon.client.APIResponse;
 import fr.gouv.etalab.mastodon.client.Entities.Account;
 import fr.gouv.etalab.mastodon.client.Entities.Conversation;
 import fr.gouv.etalab.mastodon.client.Entities.TagTimeline;
+import fr.gouv.etalab.mastodon.client.GNUAPI;
 import fr.gouv.etalab.mastodon.interfaces.OnRetrieveMissingFeedsInterface;
 import fr.gouv.etalab.mastodon.sqlite.SearchDAO;
 import fr.gouv.etalab.mastodon.sqlite.Sqlite;
@@ -67,60 +68,108 @@ public class RetrieveMissingFeedsAsyncTask extends AsyncTask<Void, Void, Void> {
     protected Void doInBackground(Void... params) {
         if( this.contextReference.get() == null)
             return null;
-        API api = new API(this.contextReference.get());
-        List<fr.gouv.etalab.mastodon.client.Entities.Status> tempStatus = null;
-        APIResponse apiResponse = null;
-        if( type == RetrieveFeedsAsyncTask.Type.HOME) {
-            apiResponse = api.getHomeTimelineSinceId(since_id);
-        }else if( type == RetrieveFeedsAsyncTask.Type.DIRECT)
-            apiResponse = api.getDirectTimelineSinceId(since_id);
-        else if( type == RetrieveFeedsAsyncTask.Type.CONVERSATION)
-            apiResponse = api.getConversationTimelineSinceId(since_id);
-        else if( type == RetrieveFeedsAsyncTask.Type.LOCAL)
-            apiResponse = api.getPublicTimelineSinceId(true, since_id);
-        else if( type == RetrieveFeedsAsyncTask.Type.PUBLIC)
-            apiResponse = api.getPublicTimelineSinceId(false, since_id);
-        else if (type == RetrieveFeedsAsyncTask.Type.REMOTE_INSTANCE)
-            apiResponse = api.getInstanceTimelineSinceId(remoteInstance, since_id);
-        else if (type == RetrieveFeedsAsyncTask.Type.TAG) {
-            SQLiteDatabase db = Sqlite.getInstance(contextReference.get(), Sqlite.DB_NAME, null, Sqlite.DB_VERSION).open();
-            List<TagTimeline> tagTimelines = new SearchDAO(contextReference.get(), db).getTimelineInfo(remoteInstance);
-            if( tagTimelines != null && tagTimelines.size() > 0){
-                TagTimeline tagTimeline = tagTimelines.get(0);
-                boolean isArt = tagTimeline.isART();
-                if( isArt)
-                    apiResponse = api.getCustomArtTimelineSinceId(false, remoteInstance, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
-                else
-                    apiResponse = api.getPublicTimelineTagSinceId(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
-            }else{
-                apiResponse = api.getPublicTimelineTag(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
-            }
-        }else if (type == RetrieveFeedsAsyncTask.Type.ART)
-            apiResponse = api.getArtTimelineSinceId( false, since_id, null, null, null);
-        if (apiResponse != null) {
-            if( type != RetrieveFeedsAsyncTask.Type.CONVERSATION)
-                tempStatus = apiResponse.getStatuses();
-            else{
-                List<Conversation> conversations = apiResponse.getConversations();
-                tempStatus = new ArrayList<>();
-                if( conversations != null && conversations.size() > 0){
-                    for(Conversation conversation: conversations){
-                        fr.gouv.etalab.mastodon.client.Entities.Status status = conversation.getLast_status();
-                        List<String> ppConversation = new ArrayList<>();
-                        for (Account account : conversation.getAccounts())
-                            ppConversation.add(account.getAvatar());
-                        status.setConversationProfilePicture(ppConversation);
-                        status.setConversationId(conversation.getId());
-                        tempStatus.add(status);
+        if(MainActivity.social != UpdateAccountInfoAsyncTask.SOCIAL.GNU && MainActivity.social != UpdateAccountInfoAsyncTask.SOCIAL.FRIENDICA) {
+            API api = new API(this.contextReference.get());
+            List<fr.gouv.etalab.mastodon.client.Entities.Status> tempStatus = null;
+            APIResponse apiResponse = null;
+            if (type == RetrieveFeedsAsyncTask.Type.HOME) {
+                apiResponse = api.getHomeTimelineSinceId(since_id);
+            } else if (type == RetrieveFeedsAsyncTask.Type.DIRECT)
+                apiResponse = api.getDirectTimelineSinceId(since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.CONVERSATION)
+                apiResponse = api.getConversationTimelineSinceId(since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.LOCAL)
+                apiResponse = api.getPublicTimelineSinceId(true, since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.PUBLIC)
+                apiResponse = api.getPublicTimelineSinceId(false, since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.REMOTE_INSTANCE)
+                apiResponse = api.getInstanceTimelineSinceId(remoteInstance, since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.TAG) {
+                SQLiteDatabase db = Sqlite.getInstance(contextReference.get(), Sqlite.DB_NAME, null, Sqlite.DB_VERSION).open();
+                List<TagTimeline> tagTimelines = new SearchDAO(contextReference.get(), db).getTimelineInfo(remoteInstance);
+                if (tagTimelines != null && tagTimelines.size() > 0) {
+                    TagTimeline tagTimeline = tagTimelines.get(0);
+                    boolean isArt = tagTimeline.isART();
+                    if (isArt)
+                        apiResponse = api.getCustomArtTimelineSinceId(false, remoteInstance, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                    else
+                        apiResponse = api.getPublicTimelineTagSinceId(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                } else {
+                    apiResponse = api.getPublicTimelineTag(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                }
+            } else if (type == RetrieveFeedsAsyncTask.Type.ART)
+                apiResponse = api.getArtTimelineSinceId(false, since_id, null, null, null);
+            if (apiResponse != null) {
+                if (type != RetrieveFeedsAsyncTask.Type.CONVERSATION)
+                    tempStatus = apiResponse.getStatuses();
+                else {
+                    List<Conversation> conversations = apiResponse.getConversations();
+                    tempStatus = new ArrayList<>();
+                    if (conversations != null && conversations.size() > 0) {
+                        for (Conversation conversation : conversations) {
+                            fr.gouv.etalab.mastodon.client.Entities.Status status = conversation.getLast_status();
+                            List<String> ppConversation = new ArrayList<>();
+                            for (Account account : conversation.getAccounts())
+                                ppConversation.add(account.getAvatar());
+                            status.setConversationProfilePicture(ppConversation);
+                            status.setConversationId(conversation.getId());
+                            tempStatus.add(status);
+                        }
                     }
                 }
-            }
 
-            if( tempStatus != null)
-                statuses.addAll(0, tempStatus);
-        }
-        if (type == RetrieveFeedsAsyncTask.Type.HOME && statuses.size() > 0) {
-            MainActivity.lastHomeId = statuses.get(0).getId();
+                if (tempStatus != null)
+                    statuses.addAll(0, tempStatus);
+            }
+            if (type == RetrieveFeedsAsyncTask.Type.HOME && statuses.size() > 0) {
+                MainActivity.lastHomeId = statuses.get(0).getId();
+            }
+        }else{
+            GNUAPI gnuapi = new GNUAPI(this.contextReference.get());
+            List<fr.gouv.etalab.mastodon.client.Entities.Status> tempStatus = null;
+            APIResponse apiResponse = null;
+            if (type == RetrieveFeedsAsyncTask.Type.GNU_HOME) {
+                apiResponse = gnuapi.getHomeTimelineSinceId(since_id);
+            }else if (type == RetrieveFeedsAsyncTask.Type.GNU_LOCAL)
+                apiResponse = gnuapi.getPublicTimelineSinceId(true, since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.GNU_WHOLE)
+                apiResponse = gnuapi.getPublicTimelineSinceId(false, since_id);
+            else if (type == RetrieveFeedsAsyncTask.Type.GNU_TAG) {
+                SQLiteDatabase db = Sqlite.getInstance(contextReference.get(), Sqlite.DB_NAME, null, Sqlite.DB_VERSION).open();
+                List<TagTimeline> tagTimelines = new SearchDAO(contextReference.get(), db).getTimelineInfo(remoteInstance);
+                if (tagTimelines != null && tagTimelines.size() > 0) {
+                    TagTimeline tagTimeline = tagTimelines.get(0);
+                    boolean isArt = tagTimeline.isART();
+                    if (isArt)
+                        apiResponse = gnuapi.getCustomArtTimelineSinceId(false, remoteInstance, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                    else
+                        apiResponse = gnuapi.getPublicTimelineTagSinceId(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                } else {
+                    apiResponse = gnuapi.getPublicTimelineTag(remoteInstance, false, since_id, tagTimelines.get(0).getAny(), tagTimelines.get(0).getAll(), tagTimelines.get(0).getNone());
+                }
+            }
+            if (apiResponse != null) {
+                if (type != RetrieveFeedsAsyncTask.Type.CONVERSATION)
+                    tempStatus = apiResponse.getStatuses();
+                else {
+                    List<Conversation> conversations = apiResponse.getConversations();
+                    tempStatus = new ArrayList<>();
+                    if (conversations != null && conversations.size() > 0) {
+                        for (Conversation conversation : conversations) {
+                            fr.gouv.etalab.mastodon.client.Entities.Status status = conversation.getLast_status();
+                            List<String> ppConversation = new ArrayList<>();
+                            for (Account account : conversation.getAccounts())
+                                ppConversation.add(account.getAvatar());
+                            status.setConversationProfilePicture(ppConversation);
+                            status.setConversationId(conversation.getId());
+                            tempStatus.add(status);
+                        }
+                    }
+                }
+
+                if (tempStatus != null)
+                    statuses.addAll(0, tempStatus);
+            }
         }
         return null;
     }
