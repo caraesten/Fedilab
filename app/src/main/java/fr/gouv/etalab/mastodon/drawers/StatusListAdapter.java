cf874f6ef2cf9fef1dba8652c07ab1be3290c066
tom79 <tschneider.ac@gmail.com>
2019-03-31 09:52:35 +0200

    poll changes

--- app/src/main/java/fr/gouv/etalab/mastodon/drawers/StatusListAdapter.java
+++ app/src/main/java/fr/gouv/etalab/mastodon/drawers/StatusListAdapter.java
@@ -576,7 +576,11 @@ public class StatusListAdapter extends RecyclerView.Adapter implements OnPostAct
                                 bar.setHeight1(30);
                                 items.add(bar);
                             }else {
-                                BarItem bar = new BarItem(pollOption.getTitle(), value, "%", ContextCompat.getColor(context, R.color.mastodonC2), Color.WHITE);
+                                BarItem bar;
+                                if( theme == Helper.THEME_LIGHT)
+                                    bar = new BarItem(pollOption.getTitle(), value, "%", ContextCompat.getColor(context, R.color.mastodonC2), Color.BLACK);
+                                else
+                                    bar = new BarItem(pollOption.getTitle(), value, "%", ContextCompat.getColor(context, R.color.mastodonC2), Color.WHITE);
                                 bar.setRounded(true);
                                 bar.setHeight1(30);
                                 items.add(bar);
