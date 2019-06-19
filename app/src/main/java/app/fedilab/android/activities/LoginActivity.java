ea469736eb1e0134ff16ef1a22f24c55c71e7f74
tom79 <tschneider.ac@gmail.com>
2019-06-19 21:37:47 +0200

    Remove Gab block inside the app.

--- app/src/main/java/app/fedilab/android/activities/LoginActivity.java
+++ app/src/main/java/app/fedilab/android/activities/LoginActivity.java
@@ -295,10 +295,6 @@ public class LoginActivity extends BaseActivity {
                         return;
                     }
                     instance = login_instance.getText().toString().trim().toLowerCase();
-                    if(instance.endsWith(".gab.com") || instance.equals("gab.com") || instance.endsWith(".gab.ai") || instance.equals("gab.ai")){
-                        Toasty.error(LoginActivity.this,getString(R.string.client_error), Toast.LENGTH_LONG).show();
-                        return;
-                    }
                     connect_button.setEnabled(false);
                     new Thread(new Runnable(){
                         @Override
