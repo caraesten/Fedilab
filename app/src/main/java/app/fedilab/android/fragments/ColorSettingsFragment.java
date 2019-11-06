c956d44a3b9c38ff47432555a96dc1d2fb0a78ff
tom79 <tschneider.ac@gmail.com>
2019-11-06 15:52:39 +0100

    Allow to change colors

--- app/src/main/java/app/fedilab/android/fragments/ColorSettingsFragment.java
+++ app/src/main/java/app/fedilab/android/fragments/ColorSettingsFragment.java
@@ -1,30 +1,69 @@
 package app.fedilab.android.fragments;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-
-import org.jetbrains.annotations.NotNull;
+import androidx.preference.PreferenceManager;
 
 import app.fedilab.android.R;
+import app.fedilab.android.helper.Helper;
+
 
 public class ColorSettingsFragment  extends PreferenceFragmentCompat {
+
+
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
-        // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.fragment_settings_color);
-    }
 
+        Preference button = findPreference("reset_pref");
+        FragmentActivity context = getActivity();
+        int style;
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int theme = sharedpreferences.getInt(Helper.SET_THEME, Helper.THEME_DARK);
+        if (theme == Helper.THEME_DARK) {
+            style = R.style.DialogDark;
+        } else if (theme == Helper.THEME_BLACK) {
+            style = R.style.DialogBlack;
+        } else {
+            style = R.style.Dialog;
+        }
+        PreferenceFragmentCompat preferenceFragmentCompat = this;
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context, style);
+                dialogBuilder.setMessage(R.string.reset_color);
+                dialogBuilder.setPositiveButton(R.string.reset, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.remove("theme_boost_header_color");
+                        editor.remove("theme_statuses_color");
+                        editor.remove("theme_link_color");
+                        editor.commit();
+                        dialog.dismiss();
+                        setPreferenceScreen(null);
+                        addPreferencesFromResource(R.xml.fragment_settings_color);
 
-    @Override
-    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState)
-    {
-        super.onViewCreated(view, savedInstanceState);
-
+                    }
+                });
+                dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.setCancelable(false);
+                alertDialog.show();
+                return true;
+            }
+        });
 
     }
 }
